ae95fe23467095526d6c0cb9c707ab6590041721
Milan Gajic <milangajic91@gmail.com>
2018-06-25 23:49:43 +0200

    Vala:It writes the result

--- src/main/java/com/vala/places/places/verticles/ServerVerticle.java
+++ src/main/java/com/vala/places/places/verticles/ServerVerticle.java
@@ -3,6 +3,7 @@ package com.vala.places.places.verticles;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import org.springframework.stereotype.Component;
@@ -56,8 +57,8 @@ public class ServerVerticle extends AbstractVerticle {
         Param components = new Param("components").value("country:" + country);
         List<Prediction> placePredictions = client.getPlacePredictions(text, components);
 
-        routingContext.response().putHeader("content-type", "text/html")
-                .end(text + "<h1>Hello from my first Vert.x 3 application</h1>" + country);
+        routingContext.response().putHeader("content-type", "text/json")
+                .end(Json.encodePrettily(placePredictions));
     }
 
 }
