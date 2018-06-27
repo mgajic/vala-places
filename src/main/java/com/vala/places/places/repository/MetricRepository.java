86b5cf22534fc7b15d2269a48637d0bb63c1ef6e
Milan Gajic <milangajic91@gmail.com>
2018-06-27 23:23:34 +0200

    Vala:Refactoring

--- src/main/java/com/vala/places/places/repository/MetricRepository.java
+++ src/main/java/com/vala/places/places/repository/MetricRepository.java
@@ -3,8 +3,6 @@ package com.vala.places.places.repository;
 import com.vala.places.places.entity.Metric;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface MetricRepository  extends JpaRepository<Metric, Long> {
+public interface MetricRepository extends JpaRepository<Metric, Long> {
 
 }
